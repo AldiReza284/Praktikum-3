ac484e89134546bc1d31c0c672dc96ab2349ba9a
AldiReza284 <aldirza284@gmail.com>
2025-11-28 22:29:14 +0700

    Add files via upload

---
+++ Modul 5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "masukkan jumlah baris segitiga pascal: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+
+        int val = 1;
+
+        for (int s = 0; s < n - i; s++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++) {
+            cout << val << " ";
+            val = val * (i - j) / (j + 1);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
